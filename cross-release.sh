86e799d1f20be72a95ef1d6247664f8b72b3d2c0
raspi <pekka.jarvinen@gmail.com>
2023-04-01 22:26:35 +0300

    fixes

--- cross-release.sh
+++ cross-release.sh
@@ -2,15 +2,29 @@
 
 # Uses cross, See: https://github.com/cross-rs/cross
 
+EXE="lajittelia"
+
 CROSSBIN="$HOME/.cargo/bin/cross"
 CROSSARGS="build --release"
 
 # https://github.com/cross-rs/cross#supported-targets
-ARCHS="x86_64-unknown-linux-gnu x86_64-unknown-freebsd x86_64-unknown-netbsd powerpc64-unknown-linux-gnu powerpc64le-unknown-linux-gnu aarch64-unknown-linux-gnu arm-unknown-linux-gnueabi"
+ARCHS="x86_64-unknown-linux-gnu x86_64-unknown-netbsd powerpc64-unknown-linux-gnu powerpc64le-unknown-linux-gnu aarch64-unknown-linux-gnu arm-unknown-linux-gnueabi x86_64-pc-windows-gnu"
 
 for t in $ARCHS
 do
+  echo "ARCH: $t -------------------------------------------"
   $CROSSBIN $CROSSARGS --target "$t"
+
+  exe=$EXE
+  if [ "$t" == "x86_64-pc-windows-gnu" ]; then
+    exe="${exe}.exe"
+  fi
+
+  upx -9 "target/$t/release/$exe"
+
+  echo ""
+  echo "--------------------------------------------"
+  echo ""
 done
 
 # copy common files
@@ -28,7 +42,12 @@ mkdir "release/v$VERSION"
 for t in $ARCHS
 do
   pushd "target/$t/release" || return
-  tar --numeric-owner --owner=0 --group=0 -zcvf "../../../release/v$VERSION/lajittelia-v$VERSION-$t.tar.gz" LICENSE README.md lajittelia
+  exe=$EXE
+  if [ "$t" == "x86_64-pc-windows-gnu" ]; then
+    exe="${exe}.exe"
+  fi
+
+  tar --numeric-owner --owner=0 --group=0 -zcf "../../../release/v$VERSION/$EXE-v$VERSION-$t.tar.gz" LICENSE README.md "$exe"
   popd || return
 done
 
